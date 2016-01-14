8c725bc80d1c20496735a5e8a6bfcd085cc602a5
Liang Bo Wang <ccwang002@gmail.com>
2016-01-14 16:13:39 +0800

    Drop probe value table

--- src/datasets/models.py
+++ src/datasets/models.py
@@ -63,10 +63,6 @@ class Sample(models.Model):
     name = models.CharField(
         max_length=1024,
     )
-    filename = models.CharField(
-        max_length=512,
-        blank=True
-    )
     cell_line = models.ForeignKey(
         'CellLine',
         related_name='samples',
@@ -92,51 +88,47 @@ class Sample(models.Model):
         return "{0.name} {0.cell_line} from {0.dataset}".format(self)
 
 
+class Gene(models.Model):
+
+    entrez_id = models.IntegerField(
+        null=True,
+        blank=True,
+        unique=True,
+    )
+
+    gene_symbol = models.CharField(
+        max_length=256,
+        blank=True,
+    )
+
+    gene_name = models.TextField(
+        blank=True,
+    )
+
+
 class ProbeInfo(models.Model):
 
     identifier = models.CharField(
         max_length=256,
     )
+
     platform = models.ForeignKey(
         'MicroarrayPlatform',
+        on_delete=models.CASCADE,
         related_name='probes',
     )
-    gene_symbol = models.CharField(
-        max_length=256,
-        blank=True,
-    )
-    gene_name = models.TextField(
-        blank=True,
-    )
-    entrez_id = models.IntegerField(
-        null=True, blank=True,
+
+    genes = models.ManyToManyField(
+        "Gene",
+        related_name="probes",
     )
 
     class Meta:
         unique_together = (
-            ("platform", "identifier", "gene_symbol",),
+            ("platform", "identifier"),
         )
 
     def __str__(self):
         return '{0.identifier} of {0.platform}'.format(self)
 
 
-class ProbeValue(models.Model):
-
-    sample = models.ForeignKey(
-        'Sample',
-        related_name='probe_values',
-        on_delete=models.CASCADE,
-    )
-    probe_id = models.CharField(
-        max_length=256,
-    )
-    value = models.FloatField(blank=True)
-
-    class Meta:
-        unique_together = (
-            ('sample', 'probe_id'),
-        )
-
-    def __str__(self):
-        return '{0.probe_id}={0.value:.2f}'.format(self)
